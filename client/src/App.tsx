3e47eac98ee151f998cde7b5941d7fca76527cf8
Alex Estrada <prodigybrazilianjj@gmail.com>
2026-01-22 13:32:26 -0700

    Fix manage-subscription flow and clean up routing imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { CartProvider } from "@/lib/cart";
 
@@ -11,7 +11,7 @@ import Product from "@/pages/Product";
 import Cart from "@/pages/Cart";
 import Checkout from "@/pages/Checkout";
 import OrderSuccess from "@/pages/OrderSuccess";
-import ManageSubscription from "./pages/ManageSubscription";
+import ManageSubscription from "@/pages/ManageSubscription";
 
 import Terms from "@/pages/Terms";
 import Privacy from "@/pages/Privacy";
@@ -28,14 +28,13 @@ function Router() {
       <Route path="/product" component={Product} />
       <Route path="/cart" component={Cart} />
       <Route path="/checkout" component={Checkout} />
+      <Route path="/order-success" component={OrderSuccess} />
       <Route path="/manage-subscription" component={ManageSubscription} />
-
 
       {/* Legal */}
       <Route path="/terms" component={Terms} />
       <Route path="/privacy" component={Privacy} />
       <Route path="/refunds" component={Refunds} />
-      <Route path="/order-success" component={OrderSuccess} />
 
       <Route component={NotFound} />
     </Switch>
